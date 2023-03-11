dda48b75ab9726e35a361757193a98cc655812e5
vasiliy <vasya-timkin@mail.ru>
2023-03-11 12:15:08 +0300

    dz2.2

--- zadanie2.cs
+++ zadanie2.cs
@@ -1,1 +1,19 @@
-﻿
+﻿int max = 0;
+int min = 0;
+Console.Write("Введите первое число: ");
+int Number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int Number2 = Convert.ToInt32(Console.ReadLine());
+
+if(Number1 > Number2)
+{
+    max = Number1;
+    min = Number2;
+}
+else
+{
+    min = Number1;
+    max = Number2;
+}
+Console.WriteLine("max = " + max + " и " + "min = " + min);
